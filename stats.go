5e694db1918adb2cd2fbeafa1b4a367990039406
antipathique <nope@gmail.com>
2017-01-31 23:31:00 +0100

    Periodic monitoring of stats

--- stats.go
+++ stats.go
@@ -3,21 +3,21 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/gregdel/pushover"
 )
 
-func monitorStats(conf Config, tracker GazelleTracker, notification *pushover.Pushover, recipient *pushover.Recipient) {
 
-	// TODO loop: select with ticker & done/stop chans
-
+func getStats(tracker GazelleTracker, notification *pushover.Pushover, recipient *pushover.Recipient) {
 	stats, err := tracker.GetStats()
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
+		log.Println(stats.Stats())
+		// TODO save to CSV?
 
 		// TODO: if something is wrong, send notif and stop
-
 		// send notification
 		message := pushover.NewMessageWithTitle("Current stats: "+stats.Stats(), "varroa musica")
 		_, err := notification.SendMessage(message, recipient)
@@ -25,6 +25,19 @@ func monitorStats(conf Config, tracker GazelleTracker, notification *pushover.Pu
 			log.Println(err.Error())
 		}
 	}
+}
 
-	fmt.Println("End of monitor")
+func monitorStats(conf Config, tracker GazelleTracker, notification *pushover.Pushover, recipient *pushover.Recipient) {
+	tickChan := time.NewTicker(time.Hour).C
+	for {
+		select {
+		case <-tickChan:
+			log.Println("Getting stats...")
+			getStats(tracker, notification, recipient)
+		case <-done:
+			return
+		case <-stop:
+			return
+		}
+	}
 }
