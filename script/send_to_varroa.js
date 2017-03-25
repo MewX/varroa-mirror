0fc5b27657ed368fc14afdeafa235a2c995b0f6c
antipathique <nope@gmail.com>
2017-03-25 10:16:46 +0100

    Separated hostname and port in the script settings for ease of use.

--- script/send_to_varroa.js
+++ script/send_to_varroa.js
@@ -3,8 +3,8 @@
 // @namespace      varroa
 // @description    Adds a VM link for each torrent, to send directly to varroa musica.
 // @include        http*://*redacted.ch/*
-// @version        1
-// @date           2017-03.27
+// @version        2
+// @date           2017-03
 // @grant          GM_getValue
 // @grant          GM_setValue
 // @grant          GM_notification
@@ -16,7 +16,7 @@ var divider = ' | ';
 var settings = getSettings();
 var settingsPage = window.location.href.match('user.php\\?action=edit&userid=');
 
-if (settings.token && settings.weblink) {
+if (settings.token && settings.url && settings.port) {
 	alltorrents = [];
 	for (var i = 0; i < document.links.length; i++) {
 		alltorrents.push(document.links[i]);
@@ -33,7 +33,8 @@ if (settings.token && settings.weblink) {
 if (settingsPage) {
 	appendSettings();
 	document.getElementById('varroa_settings_token').addEventListener('change', saveSettings, false);
-	document.getElementById('varroa_settings_weblink').addEventListener('change', saveSettings, false);
+	document.getElementById('varroa_settings_url').addEventListener('change', saveSettings, false);
+	document.getElementById('varroa_settings_port').addEventListener('change', saveSettings, false);
 }
 
 if (!settings && !settingsPage) {
@@ -49,10 +50,9 @@ function createLink(linkelement, id) {
 	link.appendChild(document.createElement("a"));
 	link.firstChild.appendChild(document.createTextNode("VM"));
 	link.appendChild(document.createTextNode(divider));
-	link.firstChild.href = settings.weblink + id + "?token=" + settings.token;
-
+	link.firstChild.href = settings.url + ":" + settings.port + "/get/" + id + "?token=" + settings.token;
 	link.firstChild.target = "_blank";
-	link.firstChild.title = "Direct Download to varroa musica";
+	link.firstChild.title = "Direct download to varroa musica";
 	linkelement.parentNode.insertBefore(link, linkelement);
 }
 
@@ -60,9 +60,10 @@ function appendSettings() {
 	var container = document.getElementsByClassName('main_column')[0];
 	var lastTable = container.lastElementChild;
 	var settingsHTML = '<a name="varroa_settings"></a>\n<table cellpadding="6" cellspacing="1" border="0" width="100%" class="layout border user_options" id="varroa_settings">\n';
-	settingsHTML += '<tbody>\n<tr class="colhead_dark"><td colspan="2"><strong>Varroa Musica Settings</strong></td></tr>\n';
+	settingsHTML += '<tbody>\n<tr class="colhead_dark"><td colspan="2"><strong>Varroa Musica Settings (autosaved)</strong></td></tr>\n';
 	settingsHTML += '<tr><td class="label" title="Token set in varroa">Token</td><td><input type="text" id="varroa_settings_token" placeholder="insert_your_token" value="' + GM_getValue('token', '') + '"></td></tr>\n';
-	settingsHTML += '<tr><td class="label" title="Your seedbox hostname + port set in varroa">Web Link</td><td><input type="text" id="varroa_settings_weblink" placeholder="http://hostname:your_chosen_port/get/" value="' + GM_getValue('weblink', '') + '"></td></tr>\n';
+	settingsHTML += '<tr><td class="label" title="Your seedbox hostname set in varroa">Hostname</td><td><input type="text" id="varroa_settings_url" placeholder="http://hostname.com" value="' + GM_getValue('url', '') + '"></td></tr>\n';
+	settingsHTML += '<tr><td class="label" title="Your seedbox port set in varroa">Port</td><td><input type="text" id="varroa_settings_port" placeholder="your_chosen_port" value="' + GM_getValue('port', '') + '"></td></tr>\n';
 	settingsHTML += '</tbody>\n</table>';
 	lastTable.insertAdjacentHTML('afterend', settingsHTML);
 
@@ -75,11 +76,13 @@ function appendSettings() {
 
 function getSettings() {
 	var token = GM_getValue('token', '');
-	var weblink = GM_getValue('weblink', '');
-	if (token && weblink) {
+	var url = GM_getValue('url', '');
+	var port = GM_getValue('port', '');
+	if (token && url && port) {
 		return {
 			token: token,
-			weblink: weblink
+			url: url,
+            port: port
 		};
 	} else {
 		return false;
